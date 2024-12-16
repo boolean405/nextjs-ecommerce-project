b9c2a8343c27b5c62dcd0aef9b57d75c99c61a3d
Boolean <boolean05@gmail.com>
2024-12-16 14:24:59 +0700

    filter products

--- client/actions/get-product.tsx
+++ client/actions/get-product.tsx
@@ -2,10 +2,9 @@ import { Product } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-
 const getProduct = async (id: string): Promise<Product> => {
   const res = await fetch(`${URL}/${id}`);
-  
+
   return res.json();
 };
 
